9c8bf26dfab511e95a003cc12e6d7510650a34ad
Nicolas <nicobou60@gmail.com>
2026-03-10 21:37:33 +0100

    spring entities

---
+++ graph-engine-backend/src/main/java/com/graphengine/graph_engine/GraphEngineApplication.java
@@ -0,0 +1,13 @@
+package com.graphengine.graph_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphEngineApplication.class, args);
+	}
+
+}
